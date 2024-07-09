6c0bb56720b99fed9d761e7225da4865d736fc74
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-10 00:25:07 +0200

    Update check_accessable.js

--- check_accessable.js
+++ check_accessable.js
@@ -1,17 +1,13 @@
-var shouldRedirect = true;
+var shouldRedirect = false;
 
 window.onload = function() {
     if (!shouldRedirect) {
-        // Alle Inhalte der Seite verbergen
         document.body.innerHTML = '';
 
-        // Neues <div>-Element erstellen und zur Seite hinzufügen
         var centerContentDiv = document.createElement('div');
         centerContentDiv.className = 'center-content';
         centerContentDiv.innerHTML = '<span>&#9888;</span> Diese Website ist derzeit nicht zugänglich.';
         
-        // Das neue <div>-Element zum body hinzufügen
         document.body.appendChild(centerContentDiv);
     }
-    // Wenn shouldRedirect true ist, wird nichts gemacht und die Seite lädt wie gewohnt.
 };
