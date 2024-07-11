b5080bbca2b72f3478e22df8e717d8693f21fc41
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-11 14:21:16 +0200

    Added files

--- check_accessable.js
+++ check_accessable.js
@@ -1,16 +1,29 @@
-var shouldRedirect = false;
+var shouldRedirect = true;
 
 window.onload = function() {
     if (!shouldRedirect) {
-        // Alle Inhalte der Seite verbergen
         document.body.innerHTML = '';
 
-        // Neues <div>-Element erstellen und zur Seite hinzufügen
         var centerContentDiv = document.createElement('div');
         centerContentDiv.className = 'center-content';
         centerContentDiv.innerHTML = '<span>&#9888;</span> This website is not accessible right now.';
         
-        // Das neue <div>-Element zum body hinzufügen
         document.body.appendChild(centerContentDiv);
+    }
+
+    var darkModeBtn = document.createElement('button');
+    darkModeBtn.className = 'dark-mode-btn';
+    darkModeBtn.innerHTML = '<i class="fa fa-moon"></i><i class="fa fa-sun"></i>';
+
+    darkModeBtn.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        var isDarkMode = document.body.classList.contains('dark-mode');
+        localStorage.setItem('darkMode', isDarkMode);
+    });
+
+    document.body.appendChild(darkModeBtn);
+
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.add('dark-mode');
     }
 };
