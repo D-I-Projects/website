e1a5e1e58caf6e0523ff047b34b7e5897dc86b35
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-15 12:37:35 +0200

    Add files via upload

--- JS/check_accessable.js
+++ JS/check_accessable.js
@@ -1,4 +1,4 @@
-var shouldRedirect = false;
+var shouldRedirect = true;
 
 window.onload = function() {
     if (!shouldRedirect) {
@@ -9,21 +9,5 @@ window.onload = function() {
         centerContentDiv.innerHTML = '<span>&#9888;</span> This website is not accessible right now.';
         
         document.body.appendChild(centerContentDiv);
-    }
-
-    var darkModeBtn = document.createElement('button');
-    darkModeBtn.className = 'dark-mode-btn';
-    darkModeBtn.innerHTML = '<i class="fa fa-moon"></i><i class="fa fa-sun"></i>';
-
-    darkModeBtn.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-        var isDarkMode = document.body.classList.contains('dark-mode');
-        localStorage.setItem('darkMode', isDarkMode);
-    });
-
-    document.body.appendChild(darkModeBtn);
-
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
     }
 };
