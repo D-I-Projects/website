07e3b4bdadf69ff6b0174a63d10519634b5d1849
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 22:24:08 +0200

    Added another console message

--- JS/welcome_console_message.js
+++ JS/welcome_console_message.js
@@ -1,3 +1,4 @@
+console.log("This Website is hosted with GHP (GitHub Pages)")
 console.log("Note: This page contains raw information only. There are no external links or redirections, and no downloads or additional content are provided. This is purely raw text.");
 console.log("Made by D&I")
 console.log("-------------------------")
