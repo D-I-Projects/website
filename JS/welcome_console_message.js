a97d0529e962d5e9a181edb0d82cbb9052c6f488
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 22:46:48 +0200

    Fixed

--- JS/welcome_console_message.js
+++ JS/welcome_console_message.js
@@ -1,5 +1,5 @@
 console.log("This Website is hosted with GHP (GitHub Pages)")
 console.log("Note: This page contains raw information only. There are no external links or redirections, and no downloads or additional content are provided. This is purely raw text.");
 console.log("Made by D&I")
-console.log("Version : v2024.07.19-DI-(3)")
+console.log("Version : v2024.07.19-DI-(4)")
 console.log("-------------------------")
