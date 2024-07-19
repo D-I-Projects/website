ac71a3660c989e66fd04533aece439e6e60a4017
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 22:20:56 +0200

    Create welcome_console_message.js

---
+++ JS/welcome_console_message.js
@@ -0,0 +1,3 @@
+console.log("Note: This page contains raw information only. There are no external links or redirections, and no downloads or additional content are provided. This is purely raw text.");
+console.log("Made by D&I")
+console.log("-------------------------")
