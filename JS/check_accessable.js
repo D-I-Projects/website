cf1c8f21d2e36185b9f52057eaedc622bdc9d7ab
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 21:09:39 +0200

    Restore

--- JS/check_accessable.js
+++ JS/check_accessable.js
@@ -10,20 +10,4 @@ window.onload = function() {
         
         document.body.appendChild(centerContentDiv);
     }
-
-    var darkModeBtn = document.createElement('button');
-    darkModeBtn.className = 'dark-mode-btn';
-    darkModeBtn.innerHTML = '<i class="fa fa-moon"></i><i class="fa fa-sun"></i>';
-
-    darkModeBtn.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-        var isDarkMode = document.body.classList.contains('dark-mode');
-        localStorage.setItem('darkMode', isDarkMode);
-    });
-
-    document.body.appendChild(darkModeBtn);
-
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-    }
 };
