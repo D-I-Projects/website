a43c901db1db9874aa7f4706355e4b3088a88363
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 20:57:45 +0200

    Add files via upload
    Signed-off-by: wfxey <158351052+wfxey@users.noreply.github.com>

--- JS/redirectProject.js
+++ JS/redirectProject.js
@@ -6,3 +6,18 @@ function redirectToDiscordBotCreator() {
     location.href = 'https://d-i-projects.github.io/website/project/discordbotcreator';
 }
 
+function redirectToDevplaceo() {
+    location.href = 'https://d-i-projects.github.io/website/project/devplaceo';
+}
+
+function redirectToDiec() {
+    location.href = 'https://d-i-projects.github.io/website/project/diec';
+}
+
+function redirectToDiscordBot() {
+    location.href = 'https://d-i-projects.github.io/website/project/discord-bot';
+}
+
+function redirectToChatBox() {
+    location.href = 'https://d-i-projects.github.io/website/project/ChatBox';
+}
