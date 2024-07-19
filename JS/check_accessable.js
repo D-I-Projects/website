818f8d433b0b859cfe25925fc5c442f564fa4a66
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-19 21:46:57 +0200

    Update check_accessable.js

--- JS/check_accessable.js
+++ JS/check_accessable.js
@@ -6,8 +6,10 @@ window.onload = function() {
 
         var centerContentDiv = document.createElement('div');
         centerContentDiv.className = 'center-content';
-        centerContentDiv.innerHTML = '<span>&#9888;</span> This website is not accessible right now.';
-        centerContentDiv.innerHTML = '<span>&#9888;</span> Reason : Updating the website';
+        
+        // Concatenate the strings to display both messages
+        centerContentDiv.innerHTML = '<span>&#9888;</span> This website is not accessible right now.<br>' +
+                                     '<span>&#9888;</span> Reason : Updating the website';
         
         document.body.appendChild(centerContentDiv);
     }
