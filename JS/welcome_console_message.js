9a3ccd7c9ef06b7b59d3f8cc74a9b0adfc46bcd7
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-23 14:13:48 +0200

    Add files via upload
    Signed-off-by: wfxey <158351052+wfxey@users.noreply.github.com>

--- JS/welcome_console_message.js
+++ JS/welcome_console_message.js
@@ -1,5 +1,5 @@
 console.log("This Website is hosted with GHP (GitHub Pages)")
 console.log("Note: This page contains raw information only. There are no external links or redirections, and no downloads or additional content are provided. This is purely raw text.");
 console.log("Made by D&I")
-console.log("Version : v2024.07.23-DI-(1)")
+console.log("Version : v2024.07.23-DI-(2)")
 console.log("-------------------------")
